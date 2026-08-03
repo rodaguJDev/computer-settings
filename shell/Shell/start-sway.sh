1f9317318077cd29095bf7a8299462a29413c5ed
eduardosaraujo1 <eduardosaraujo1@proton.me>
2026-08-03 10:59:19 -0300

    arch: setup qt dark theme and add dolphin

--- shell/Shell/start-sway.sh
+++ shell/Shell/start-sway.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
+export QT_QPA_PLATFORMTHEME=qt6ct
+export QT_QPA_PLATFORM=wayland
+
 if [ -z "$WAYLAND_DISPLAY" ] && [ -n "$XDG_VTNR" ] && [ "$XDG_VTNR" -eq 1 ] ; then
     exec sway
 fi
